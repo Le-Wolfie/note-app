8e353e8015a2d646bd88ad940e98e29830102112
Amr Khaled <lewolfje@gmail.com>
2024-08-28 22:21:08 +0300

    add trash page

---
+++ app/(root)/trash/page.tsx
@@ -0,0 +1,20 @@
+import Notes from "../notes/_components/Note";
+import { getNotes } from "../notes/_actions/note.actions";
+
+export default async function Page() {
+  const { notes } = await getNotes();
+  const filteredNotes = notes.filter((note: any) => note.markedForDeletion);
+  return (
+    <>
+      <div className='flex flex-col justify-center items-center'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 p-4'>
+          {filteredNotes.length > 0
+            ? filteredNotes.map((note: any) => (
+                <Notes key={note._id} note={note} />
+              ))
+            : "You have no notes in the trash."}
+        </div>
+      </div>
+    </>
+  );
+}
