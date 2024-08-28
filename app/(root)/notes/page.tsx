f13520a13402dddcad08c27b6ce7f3145d70fc93
Amr Khaled <lewolfje@gmail.com>
2024-08-28 22:19:42 +0300

    filter archived and deleted notes in /notes

---
+++ app/(root)/notes/page.tsx
@@ -0,0 +1,26 @@
+import { getNotes } from "./_actions/note.actions";
+import CreateNoteForm from "./_components/CreateNoteForm";
+import Notes from "./_components/Note";
+
+
+export default async function Page() {
+  const { notes } = await getNotes();
+  // Display only the notes that are not archived or deleted
+  const filteredNotes = notes.filter(
+    (note: any) => !note.markedAsArchived && !note.markedForDeletion
+  );
+  return (
+    <>
+      <div className='flex flex-col justify-center items-center'>
+        <CreateNoteForm />
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 p-4'>
+          {filteredNotes.length > 0
+            ? filteredNotes.map((note: any) => (
+                <Notes key={note._id} note={note} />
+              ))
+            : "No notes found, create one!"}
+        </div>
+      </div>
+    </>
+  );
+}
