7f973c327e13e1aa3b3bfe20355ad6f8c7d24508
Amr Khaled <lewolfje@gmail.com>
2024-08-28 16:33:24 +0300

    remove note comp and clean up

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,24 +1,8 @@
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { EllipsisVertical } from "lucide-react";
-import React from "react";
 import CreateNoteForm from "./_components/CreateNoteForm";
 import { backendAPI } from "@/api";
 import { auth } from "@clerk/nextjs/server";
 import moment from "moment";
+import Notes from "./_components/Note";
 
 async function getNotes() {
   const clerkUser = auth();
@@ -41,11 +25,12 @@ export default async function Page() {
         <CreateNoteForm />
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 p-4'>
           {notes.length > 0
-            ? notes.map((note: any, index: any) => (
+            ? notes.map((note: any) => (
                 <Notes
-                  key={index}
+                  key={note._id}
                   title={note.title}
                   description={note.content}
+                  noteId={note._id}
                   footer={moment(note.updatedAt).fromNow()}
                 />
               ))
@@ -53,47 +38,5 @@ export default async function Page() {
         </div>
       </div>
     </>
-  );
-}
-
-function Notes({
-  title,
-  description,
-  footer,
-}: {
-  title: string;
-  description: string;
-  footer: string;
-}) {
-  return (
-    <Card className='relative mb-4 break-inside-avoid border-2'>
-      <CardHeader className='relative p-4 pb-6'>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button
-              size='icon'
-              variant='outline'
-              className='absolute top-1 right-1 border-none'
-            >
-              <EllipsisVertical />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuItem>Edit</DropdownMenuItem>
-            <DropdownMenuItem>Archive</DropdownMenuItem>
-            <DropdownMenuItem>Delete</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-        <CardTitle className='line-clamp-1 pr-10'>{title}</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <CardDescription className='line-clamp-2'>
-          {description}
-        </CardDescription>
-      </CardContent>
-      <CardFooter>
-        <p className='text-sm text-pretty'>Updated {footer}</p>
-      </CardFooter>
-    </Card>
   );
 }
