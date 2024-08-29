99f1d37466f799819cdd7f506123267f8547e0d4
Amr Khaled <lewolfje@gmail.com>
2024-08-29 17:48:02 +0300

    add reminder value over the note cards

--- app/(root)/reminders/page.tsx
+++ app/(root)/reminders/page.tsx
@@ -23,13 +23,24 @@ export default async function Page({
             note.reminder
         )
       : [];
+
   return (
     <>
       <div className='flex flex-col justify-center items-center'>
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 p-4'>
           {filteredNotes.length > 0
             ? filteredNotes.map((note: any) => (
-                <Notes key={note._id} note={note} />
+                <div key={note._id} className='w-full'>
+                  <div className='font-semibold p-2 rounded-t'>
+                    {new Date(note.reminder).toLocaleDateString("en-US", {
+                      weekday: "long",
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    })}
+                  </div>
+                  <Notes note={note} />
+                </div>
               ))
             : searchParams.q === ""
             ? "You have no reminders set"
